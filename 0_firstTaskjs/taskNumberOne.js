02490912303888558ac613a3a95d45f326568576
po6eptka <43238090+po6eptka@users.noreply.github.com>
2018-10-27 10:15:09 +0300

    Initial commit

---
+++ 0_firstTaskjs/taskNumberOne.js
@@ -0,0 +1,7 @@
+var sumofmany=parseInt(Number(prompt("сколько у вас денег?")));
+var hmapple=Number(prompt("сколько яблок вы купили?"));
+var hmbread=Number(prompt("сколько штук хлеба вы взяли?"));
+var sumcoinofapple=parseInt(Number(prompt("сколько стоит одно яблоко?"))*hmapple);
+var sumcoinofbread=parseInt(Number(prompt("сколько стоит один хлеб?"))*hmbread);
+var justdochoose=Boolean(sumofmany>sumcoinofapple+sumcoinofbread);
+document.write(justdochoose);
